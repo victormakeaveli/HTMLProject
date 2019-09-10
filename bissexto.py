834fd0619c7221a9c24e6f19a99bf9bcad3502fe
Victor Hugo Rocha dos Santos Grigolato <55029810+victormakeaveli@users.noreply.github.com>
2019-09-10 10:26:53 -0300

    Delete bissexto.py

--- bissexto.py
+++
@@ -1,5 +0,0 @@
-num = int(input())
-if num % 4 == 0 and num % 100 != 0 or num % 100 == 0 and num % 400 == 0:
-    print('{} é bissexto'.format(num))
-else:
-    print('{} não é bissexto'.format(num))
